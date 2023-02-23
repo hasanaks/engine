2c60819591a18ccf1b6ccf5b87bbe0ca5132af6e
hasanaks <hasanarda2015@gmail.com>
2023-02-23 20:34:21 +0300

    add an example horizontally launched particle

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <raylib.h>
 
+#include "World.hpp"
+
 int main() {
-  InitWindow(120, 180, "Engine");
+  InitWindow(800, 600, "Engine");
+
+  SetTargetFPS(60);
+
+  World world;
+  auto particle = std::make_shared<Particle>();
+  particle->mass = 100;
+  world.AddParticle(particle);
 
   while (!WindowShouldClose()) {
+	world.Step(GetFrameTime());
+
     BeginDrawing();
 
-    ClearBackground(RAYWHITE);
-    DrawRectangle(0, 0, 100, 100, BLUE);
+    ClearBackground(BLACK);
+    DrawRectangle(particle->position.x, -particle->position.y, 60, 60, WHITE);
      
     EndDrawing();
   }
