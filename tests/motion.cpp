8b35680b7f5771b66d80d629e5248f0df65d4580
hasanaks <hasanarda2015@gmail.com>
2023-02-23 18:41:54 +0300

    update tests/motion.cpp to include Particle.hpp instead of Motion.hpp

--- tests/motion.cpp
+++ tests/motion.cpp
@@ -1,7 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 
-#include "../Motion.hpp"
+#include "../Particle.hpp"
 
 TEST_CASE("particle motion physics") {
   // TODO: write tests
