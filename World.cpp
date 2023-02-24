fb05d4cac4bd6a5e1c233833ae2116ea2fd02951
hasanaks <hasanarda2015@gmail.com>
2023-02-24 19:45:58 +0300

    move implementations of World class' functions to a source file

---
+++ World.cpp
@@ -0,0 +1,23 @@
+#include "World.hpp"
+
+void World::Step(float dt) {
+  for (auto &particle : particles) {
+    particle->force += particle->mass * gravity;
+    auto acceleration = particle->force / particle->mass;
+
+    particle->position +=
+      particle->velocity * dt + 0.5f * acceleration * dt * dt;
+    particle->velocity += acceleration * dt;
+
+    particle->force = {0, 0};
+  }
+}
+
+void World::AddParticle(std::shared_ptr<Particle> particle) {
+  particles.push_back(particle);
+}
+
+void World::RemoveParticle(std::shared_ptr<Particle> particle) {
+  particles.erase(std::remove(particles.begin(), particles.end(), particle),
+      particles.end());
+}
