762c410fa57fc45ed086b9f96766268c64b9bc71
hasanaks <hasanarda2015@gmail.com>
2023-02-24 19:59:00 +0300

    add a test case for motion physics

--- tests/motion.cpp
+++ tests/motion.cpp
@@ -1,8 +1,18 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 
-#include "../Particle.hpp"
+#include "../World.hpp"
 
 TEST_CASE("particle motion physics") {
-  // TODO: write tests
+  World world;
+
+  auto particle = std::make_shared<Particle>();
+  particle->mass = 10;
+
+  world.AddParticle(particle);
+  world.Step(1);
+
+  CHECK(particle->force == Vector2{0, 0});
+  CHECK(particle->position == Vector2{0, -5});
+  CHECK(particle->velocity == Vector2{0, -10});
 }
