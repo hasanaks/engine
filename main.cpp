504959b520dfb34e44af7553f6bd6b058cac02d4
hasanaks <hasanarda2015@gmail.com>
2023-02-25 19:49:08 +0300

    add a 2d camera

--- main.cpp
+++ main.cpp
@@ -4,9 +4,16 @@
 #include "World.hpp"
 
 int main() {
-  InitWindow(800, 600, "Engine");
+  InitWindow(1200, 800, "Engine");
 
-  SetTargetFPS(60);
+  SetTargetFPS(120);
+
+  Camera2D camera = {
+	.offset{GetScreenWidth() / 2.f, GetScreenHeight() / 2.f},
+	.target{},
+	.rotation{},
+	.zoom = 1,
+  };
 
   World world;
   auto particle = std::make_shared<Particle>();
@@ -16,10 +23,29 @@ int main() {
   while (!WindowShouldClose()) {
     world.Step(GetFrameTime());
 
+	if (IsKeyDown(KEY_LEFT)) {
+	  camera.target.x -= 10;
+	}
+
+	if (IsKeyDown(KEY_RIGHT)) {
+	  camera.target.x += 10;
+	}
+
+	if (IsKeyDown(KEY_UP)) {
+	  camera.target.y -= 10;
+	}
+
+	if (IsKeyDown(KEY_DOWN)) {
+	  camera.target.y += 10;
+	}
+
     BeginDrawing();
 
     ClearBackground(BLACK);
+
+	BeginMode2D(camera);
     DrawRectangle(particle->position.x, -particle->position.y, 60, 60, WHITE);
+	BeginMode2D(camera);
 
     EndDrawing();
   }
