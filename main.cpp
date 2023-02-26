cb49e15ab13bf87e2caf0279bedd799cede1680c
hasanaks <hasanarda2015@gmail.com>
2023-02-26 13:04:34 +0300

    update physics independent of frametime

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ int main() {
   InitWindow(1200, 800, "Engine");
 
   Camera2D camera = {
-	.offset{GetScreenWidth() / 2.f, GetScreenHeight() / 2.f},
-	.target{},
-	.rotation{},
-	.zoom = 1,
+      .offset{GetScreenWidth() / 2.f, GetScreenHeight() / 2.f},
+      .target{},
+      .rotation{},
+      .zoom = 1,
   };
 
   World world;
@@ -19,32 +19,40 @@ int main() {
   particle->mass = 100;
   world.AddParticle(particle);
 
+  float accumulator = 0;
+  float physicsTimeStep = 0.01f;
+
   while (!WindowShouldClose()) {
-    world.Step(GetFrameTime());
+    accumulator += GetFrameTime();
 
-	if (IsKeyDown(KEY_LEFT)) {
-	  camera.target.x -= 10;
-	}
+    if (IsKeyDown(KEY_LEFT)) {
+      camera.target.x -= 10;
+    }
 
-	if (IsKeyDown(KEY_RIGHT)) {
-	  camera.target.x += 10;
-	}
+    if (IsKeyDown(KEY_RIGHT)) {
+      camera.target.x += 10;
+    }
 
-	if (IsKeyDown(KEY_UP)) {
-	  camera.target.y -= 10;
-	}
+    if (IsKeyDown(KEY_UP)) {
+      camera.target.y -= 10;
+    }
 
-	if (IsKeyDown(KEY_DOWN)) {
-	  camera.target.y += 10;
-	}
+    if (IsKeyDown(KEY_DOWN)) {
+      camera.target.y += 10;
+    }
+
+    while (accumulator >= physicsTimeStep) {
+      world.Step(physicsTimeStep);
+	  accumulator -= physicsTimeStep;
+    }
 
     BeginDrawing();
 
     ClearBackground(BLACK);
 
-	BeginMode2D(camera);
+    BeginMode2D(camera);
     DrawRectangle(particle->position.x, -particle->position.y, 60, 60, WHITE);
-	BeginMode2D(camera);
+    BeginMode2D(camera);
 
     EndDrawing();
   }
