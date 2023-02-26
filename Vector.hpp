a7c9f9cd36919bf7b4e6f15c2a1def80f2185afe
hasanaks <hasanarda2015@gmail.com>
2023-02-26 12:28:10 +0300

    fix declaration for the *= operator

--- Vector.hpp
+++ Vector.hpp
@@ -9,6 +9,6 @@ Vector2 operator/(const Vector2 &lhs, const float &rhs);
 Vector2 operator-(const Vector2 &rhs);
 void operator+=(Vector2 &lhs, const Vector2 &rhs);
 void operator-=(Vector2 &lhs, const Vector2 &rhs);
-void operator*=(Vector2 &lhs, const float rhs);
+void operator*=(Vector2 &lhs, const float& rhs);
 bool operator==(const Vector2 &lhs, const Vector2 &rhs);
 bool operator!=(const Vector2 &lhs, const Vector2 &rhs);
