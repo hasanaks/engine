0de967251be15f1cd51bd27d668d1cf405cce1d2
cac <cac@gmail.com>
2023-03-20 22:06:38 +0300

    icarus

--- Constraint.cpp
+++ Constraint.cpp
@@ -1,18 +1,18 @@
 #include "Constraint.hpp"
 
 
-	Constraint::Constraint(std::shared_ptr<Particle> A,Vector2f* pos) {
+	Constraint::Constraint(std::shared_ptr<Particle> A,Vector2f pos) {
 		ret = A;
 		positionA = &(A->position);
 		positionB = pos;
 		forcext = &(A->force);
 		Va = &(A->velocity);
-		*relP = (*positionA) - (*positionB);
+		relP = (*positionA) - (positionB);
 		m = A->mass;
 	}
 	void Constraint::C2() {
-	  lambda = (-forcext->dot(*relP)-(m*Va->dot(*Va))/(relP->dot(*relP)));
-		forceC = (*relP) * lambda;
+	  lambda = (-forcext->dot(relP)-(m*Va->dot(*Va))/(relP.dot(relP)));
+		forceC = (relP) * lambda;
 		ret->force += forceC;
 
 	}
--- Constraint.hpp
+++ Constraint.hpp
@@ -2,18 +2,19 @@
 
 #include <cmath>
 #include <vector>
+#include <memory>
 #include "Particle.hpp"
 
 class Constraint {
 public:
-	Constraint(std::shared_ptr<Particle> A, Vector2f* pos);
+	Constraint(std::shared_ptr<Particle> A, Vector2f pos);
 	void C2();
 private:
 	Vector2f* positionA{};
-	Vector2f* positionB{};
+	Vector2f positionB{};
 	Vector2f* Va{};
 	Vector2f* Vb{};
-	Vector2f* relP{};
+	Vector2f relP{};
 	Vector2f* forcext{};
 	float m;
 	float lambda{};
