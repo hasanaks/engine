060c1c9529de93db693cf78b2879a7d1a4b714d0
hasanaks <hasanarda2015@gmail.com>
2023-04-20 17:25:26 +0300

    add collision tests

---
+++ tests/collision.cpp
@@ -0,0 +1,9 @@
+#include "doctest.h"
+#include "../Collision.hpp"
+
+TEST_CASE("Overlap") {
+  AABB box1{nullptr, {0, 100}, {100, 0}};
+  AABB box2{nullptr, {10, 10}, {20, 20}};
+
+  CHECK(isOverlapping(&box1, &box2));
+}
