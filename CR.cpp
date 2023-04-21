cb523205427a90e1053cf7e56fdde6383e1b5f62
hasanaks <hasanarda2015@gmail.com>
2023-04-21 21:04:52 +0300

    fix Imp

--- CR.cpp
+++ CR.cpp
@@ -6,26 +6,21 @@ ImpulseSolver::ImpulseSolver(std::shared_ptr<Particle> P1,
   B = P2;
 }
 
-Vector2f ImpulseSolver::normalFinder() {
-  Vector2f relpos = A->position - B->position;
-  float a = pow(relpos.dot(relpos), 1 / 2);
-  return relpos / a;
-}
 void ImpulseSolver::Imp() {
-  Vector2f normal = normalFinder();
   /*Eigen::Vector4f Jacobian;
   Jacobian[0] = normal[0];			Will be useful when calculations
   of impulse is written only as matrix multiplication Jacobian[1] = normal[1];
   Jacobian[2] = -normal[0];
   Jacobian[3] = -normal[1];*/
 
+  Vector2f relativePosition = B->position - A->position;
+
+  Vector2f normal = relativePosition.normalized();
+  float Vd = relativePosition.dot(normal);
+
   float invm = (1 / A->mass) + (1 / B->mass);
 
-  Vector2f Vr = A->velocity - B->velocity;
-
-  float Vd = Vr.dot(normal);
-
-  float lambda = -(Vd) / invm;
+  float lambda = -Vd / invm;
 
   Vector2f aImp = normal * lambda;
   Vector2f bImp = -normal * lambda;
