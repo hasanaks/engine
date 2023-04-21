cf322a4fd429a1eae7b2a183e2e158f29fbed1a6
hasanaks <hasanarda2015@gmail.com>
2023-04-21 14:13:02 +0300

    fix collision test

--- tests/collision.cpp
+++ tests/collision.cpp
@@ -2,8 +2,8 @@
 #include "doctest.h"
 
 TEST_CASE("Overlap") {
-  AABB box1{nullptr, {0, 100}, {100, 0}};
-  AABB box2{nullptr, {10, 10}, {20, 20}};
+  AABB box1{nullptr, {0, -100}, {100, 0}};
+  AABB box2{nullptr, {10, -10}, {20, -20}};
 
   CHECK(isOverlapping(&box1, &box2));
 }
