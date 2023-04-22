87db3d133b17b95fa1d4e26e995a69d2944b5352
hasanaks <hasanarda2015@gmail.com>
2023-04-23 00:41:15 +0300

    fix 'Overlap' test

--- tests/collision.cpp
+++ tests/collision.cpp
@@ -1,13 +1,14 @@
-#include "../Collision.hpp"
 #include "doctest.h"
+
+#include "../Collision.hpp"
 
 TEST_CASE("Overlap") {
   PhysicsObject object1{};
-  object1.transform = {100, -100};
+  object1.transform = {100, 100};
 
   PhysicsObject object2{};
   object2.position = {10, -10};
-  object1.transform = {100, -100};
+  object1.transform = {100, 100};
 
   AABB box1(&object1);
   AABB box2(&object2);
